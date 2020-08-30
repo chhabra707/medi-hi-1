211f500db64265b3f5683a0b21b5ca8d3456459d
ryancho <chowin21@gmail.com>
2020-08-31 08:34:15 +0900

    first

---
+++ wp-content/themes/brand-h/footer.php
@@ -0,0 +1,235 @@
+<?php
+
+/*
+		This is the template for the footer
+		
+		@package sunsettheme
+	*/
+
+?>
+
+<footer class="text-center mt-20 font-noto-regular">
+
+
+<div class="text-2xl mb-5  ">
+<a href="https://himedi.com/en?terms=true" class="text-gray-600 font-bold "style="font-size:14px;">Terms of Use</a> .
+<a href="https://himedi.com/en?privacy=true" class="text-gray-600 font-bold" style="font-size:14px;">Privacy Policy</a>
+</div>
+
+
+<p class="text-gray-500 px-20 " style="font-size:13px">
+10, Anson Road, #34-10, International Plaza, Singapore 079903
+</p>
+
+
+<p class="text-gray-500 mt-8 mb-16">
+&copy; HIMEDI SEA PTE. LTD.
+</p>
+
+</footer>
+
+<?php wp_footer(); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script>
+		$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".header-container").addClass("fixed-top");
+    } else {
+        $(".header-container").removeClass("fixed-top");
+    }
+});
+		</script>
+
+<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script>
+	var mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		loop: false,
+
+		// If we need pagination
+		// pagination: {
+		// 	el: '.swiper-pagination',
+		// },
+
+		//Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// And if we need scrollbar
+		scrollbar: {
+			el: '.swiper-scrollbar',
+		},
+	});
+
+	jQuery(document).ready(
+		function() {
+
+			for (let i = 0; i < 3; i++) {
+				const chart = jQuery('.chart' + i);
+				const trig = jQuery('.chartTrig' + i);
+				chart.hide();
+
+				trig.on('click', function() {
+					jQuery(this).addClass('active').siblings().removeClass('active');
+					jQuery(this).addClass('border-2').siblings().removeClass('border-2');
+					if (trig.hasClass('active')) {
+						chart.addClass('active').siblings().removeClass('active');
+					}
+				});
+			}
+
+			jQuery("form input").on('invalid', function() {
+				$('input,textarea,radio').addClass('border border-red-500');
+			});
+		}
+	);
+</script>
+<script>
+
+
+
+	// start logo
+
+	// function myFunction1(){
+		
+	// const popUpBgElement = document.getElementById("pop_up_bg");
+	// const popUpElement = document.getElementById("popup");
+	// const cancelBtn = document.getElementById("popup_cancel_btn");
+	// const button = document.getElementById("click_me_btn");
+	// let isPopUpShowing = false;
+
+	// const popUpHandeler = ()=>{
+	// 	isPopUpShowing = !isPopUpShowing;
+	// 	if(isPopUpShowing){
+	// 		popUpBgElement.classList.remove("hidden");
+	// 		popUpElement.classList.remove("hidden");
+	// 		cancelBtn.classList.remove("hidden");
+	// 	}else{
+	// 		popUpBgElement.classList.add("hidden");
+	// 		popUpElement.classList.add("hidden");
+	// 		cancelBtn.classList.add("hidden");
+	// 	}
+	// }
+
+	// //** Events */
+	// button.addEventListener('click', popUpHandeler);
+	// cancelBtn.addEventListener('click', popUpHandeler);
+	// }
+
+	// function myFunction2(){
+		
+	// 	const popUpBgElement = document.getElementById("pop_up_bg");
+	// 	const cancelBtn = document.getElementById("popup_cancel_btn");
+
+	// 	const popUpElement = document.getElementById("popup2");
+	// 	const button = document.getElementById("click_me_btn2");
+	// 	let isPopUpShowing = false;
+	
+	// 	const popUpHandeler = ()=>{
+	// 		isPopUpShowing = !isPopUpShowing;
+	// 		if(isPopUpShowing){
+	// 			popUpBgElement.classList.remove("hidden");
+	// 			popUpElement.classList.remove("hidden");
+	// 			cancelBtn.classList.remove("hidden");
+	// 		}else{
+	// 			popUpBgElement.classList.add("hidden");
+	// 			popUpElement.classList.add("hidden");
+	// 			cancelBtn.classList.add("hidden");
+	// 		}
+	// 	}
+	
+	// 	//** Events */
+	// 	button.addEventListener('click', popUpHandeler);
+	// 	cancelBtn.addEventListener('click', popUpHandeler);
+	// }	
+
+	// function myFunction3(){
+		
+	// 	const popUpBgElement = document.getElementById("pop_up_bg");
+	// 	const cancelBtn = document.getElementById("popup_cancel_btn");
+
+	// 	const popUpElement = document.getElementById("popup3");
+	// 	const button = document.getElementById("click_me_btn3");
+	// 	let isPopUpShowing = false;
+	
+	// 	const popUpHandeler = ()=>{
+	// 		isPopUpShowing = !isPopUpShowing;
+	// 		if(isPopUpShowing){
+	// 			popUpBgElement.classList.remove("hidden");
+	// 			popUpElement.classList.remove("hidden");
+	// 			cancelBtn.classList.remove("hidden");
+	// 		}else{
+	// 			popUpBgElement.classList.add("hidden");
+	// 			popUpElement.classList.add("hidden");
+	// 			cancelBtn.classList.add("hidden");
+	// 		}
+	// 	}
+	
+	// 	//** Events */
+	// 	button.addEventListener('click', popUpHandeler);
+	// 	cancelBtn.addEventListener('click', popUpHandeler);
+	// }	
+
+	// function myFunction4(){
+		
+	// 	const popUpBgElement = document.getElementById("pop_up_bg");
+	// 	const cancelBtn = document.getElementById("popup_cancel_btn");
+
+	// 	const popUpElement = document.getElementById("popup4");
+	// 	const button = document.getElementById("click_me_btn4");
+	// 	let isPopUpShowing = false;
+	
+	// 	const popUpHandeler = ()=>{
+	// 		isPopUpShowing = !isPopUpShowing;
+	// 		if(isPopUpShowing){
+	// 			popUpBgElement.classList.remove("hidden");
+	// 			popUpElement.classList.remove("hidden");
+	// 			cancelBtn.classList.remove("hidden");
+	// 		}else{
+	// 			popUpBgElement.classList.add("hidden");
+	// 			popUpElement.classList.add("hidden");
+	// 			cancelBtn.classList.add("hidden");
+	// 		}
+	// 	}
+	
+	// 	//** Events */
+	// 	button.addEventListener('click', popUpHandeler);
+	// 	cancelBtn.addEventListener('click', popUpHandeler);
+	// }	
+
+// function success_cases_popup(){
+
+// 	const popUpBgElement = document.getElementById("pop_up_bg");
+// 	const popUpElement = document.getElementById("main_pop_up");
+// 	const choleithiasisButton = document.getElementById("choleithiasis_button");
+// 	let isPopUpShowing = false;
+
+// 	const popUpHandeler = ()=>{
+// 	isPopUpShowing = !isPopUpShowing;
+// 	if(isPopUpShowing){
+// 		popUpBgElement.classList.remove("hidden");
+// 		popUpElement.classList.remove("hidden");
+// 	}else{
+// 		popUpBgElement.classList.add("hidden");
+// 		popUpElement.classList.add("hidden");
+// 	}
+// 	}
+
+// 	/** Events */
+// 	choleithiasisButton.addEventListener('click', popUpHandeler);
+// 	popUpBgElement.addEventListener('click', popUpHandeler);
+
+
+
+// }	
+
+</script>
+</body>
+
+</html>
---
+++ wp-content/themes/brand-h/inc/templates/inquiry.php
@@ -0,0 +1,32 @@
+<!-- Section 4 (start) -->
+<section class="items-center pt-10">
+
+  <!-- Icon (start) -->
+  
+  <div style="margin-top:110px;">
+    <div class="border-gray-700 border w-16 h-16 rounded-full py-2 px-2 flex items-center justify-center text-gray-700" style="margin:0 auto">
+        <svg class="w-8 h-8" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="tasks" class="svg-inline--fa fa-tasks fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M145.35 207a8 8 0 0 0-11.35 0l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 250.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM62.93 384c-17.67 0-32.4 14.33-32.4 32s14.73 32 32.4 32a32 32 0 0 0 0-64zm82.42-337A8 8 0 0 0 134 47l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 90.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM503 400H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8zm0-320H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8V88a8 8 0 0 0-8-8zm0 160H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8z"></path></svg>
+      </div>
+    
+  </div>
+	<!-- Icon (end) -->
+
+	<!-- Content (start) -->
+	<h2 class="text-center px-4 font-size-24 leading-none text-gray-700" style="font-size:24px; line-height:1.2">
+    Please leave your contact information and inquiry. An answer will be given <br class="lg:hidden"><b>within 24 hours.</b>
+  </h3>
+  <p class="mt-5 text-center px-6 leading-none" style="color:#2681F3;font-size:16px">
+	
+		* If you have an urget inquiry <br class="lg:hidden"> please leave it via WhatsApp.
+	</p>
+	<!-- Content (end) -->
+
+	<!-- Divider (start) -->
+	<div class="divider mx-auto w-1/3 h-1 bg-gray-500 mt-16"></div>
+	<!-- Divider (end) -->
+
+	<!-- Form (start) -->
+	<?php echo do_shortcode("[brand-h-form]"); ?>
+	<!-- Form (end) -->
+</section>
+<!-- Section 4 (end) -->
